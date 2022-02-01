98fd46876f3f42e7607db0a5014401d84abe0148
OperationXen <OperationXen@apocryphasecurity.com>
2022-02-01 17:49:15 +0000

    Some admin page changes for readability

--- CoreServer/core/models/game.py
+++ CoreServer/core/models/game.py
@@ -8,8 +8,8 @@ class Game(models.Model):
     """ Defines an specific scheduled game """
     class GameTypes(models.TextChoices):
         """ Internal class to define possible game types """
-        RES_AL = 'Resident AL', ('Resident Adventurers League')
-        GST_AL = 'Guest AL DM', ('Guest DM Adventurers League')
+        RES_AL = 'Resident AL', ('Resident Adventurer\'s League')
+        GST_AL = 'Guest AL DM', ('Community DM Adventurer\'s League')
         EPC_AL = 'Epic AL', ('Epic Adventurers League')
         NON_AL = 'Non-AL One Shot', ('Non-AL One Shot')
         CMPGN = 'Campaign', ('Campaign')
@@ -25,8 +25,8 @@ class Game(models.Model):
         OTHER = 'Other setting', ('Any Other setting')
 
     dm = models.ForeignKey(DM, related_name='games', on_delete=models.CASCADE, help_text='Dungeon Master for this game')
-    name = models.CharField(max_length=128, help_text='User defined game name')
-    module = models.CharField(blank=True, max_length=32, help_text='AL module code')
+    name = models.CharField(max_length=128, help_text='Adventure Name')
+    module = models.CharField(blank=True, max_length=32, help_text='Adventure Code (e.g. CCC-GHC-09)')
     realm = models.TextField(max_length=16, choices=Realms.choices, default=Realms.FAERUN, help_text='Game setting')
     variant = models.TextField(max_length=16, choices=GameTypes.choices, default=GameTypes.RES_AL, help_text='Game type')
     description = models.TextField(blank=True, help_text='Description of this game or flavour text')
@@ -34,15 +34,15 @@ class Game(models.Model):
     level_min = models.IntegerField(default=1, help_text='Minumum starting level')
     level_max = models.IntegerField(default=4, help_text='Maximum player level')
     warnings = models.TextField(blank=False, default='None', help_text='Content warnings or advisories')
-    channel = models.CharField(blank=True, max_length=32, help_text='Discord channel to use for this game')
-    streaming = models.BooleanField(default=False, help_text='Game is streaming or not')
+    channel = models.CharField(blank=True, max_length=32, help_text='This field is not yet used')
+    streaming = models.BooleanField(default=False, help_text='Set to indicate that you may wish to stream the game')
 
-    datetime_release = models.DateTimeField(blank=True, null=True, help_text='Date/Time game is released for signups (your local time)')
-    datetime_open_release = models.DateTimeField(blank=True, null=True, help_text='Date/Time game is released to gen-pop (your local time)')
-    datetime = models.DateTimeField(help_text='Date/Time game is starting (your local time)')
+    datetime_release = models.DateTimeField(blank=True, null=True, help_text='Date/Time game is released for PATREON signups (your local time)', verbose_name='Patreon Release Time')
+    datetime_open_release = models.DateTimeField(blank=True, null=True, help_text='Date/Time game is released for GENERAL signups (your local time)', verbose_name='General Release Time')
+    datetime = models.DateTimeField(help_text='Date/Time game is starting (your local time)', verbose_name='Game Time')
     length = models.CharField(max_length=48, default="2 hours", blank=True, help_text='Planned duration of game')
 
-    ready = models.BooleanField(default=True, help_text='If this game is ready to be published, if this is not set the game will be hidden')
+    ready = models.BooleanField(default=True, help_text='Unset this if the game is not yet ready for release (i.e. still in draft)')
 
     def __str__(self):
         return f"{self.dm.discord_name} - {self.name}"
