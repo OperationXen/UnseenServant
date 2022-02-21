9c26dfaae3ec58a184c8a97407959a0e553e0606
OperationXen <operationxen@apocryphasecurity.com>
2022-02-21 14:22:18 +0000

    Help text refinement (#88)
    * Reverted to hacky url import for bot start
    
    * oops, reset token

--- CoreServer/config/urls.py
+++ CoreServer/config/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('discordbot', include('discordbot.startup')) 
 ]
