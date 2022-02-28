343c62078c8fc735de05f87bdb15b130b49d7fde
OperationXen <OperationXen@apocryphasecurity.com>
2022-02-28 15:39:31 +0000

    Changes to /games command

--- CoreServer/discordbot/logs.py
+++ CoreServer/discordbot/logs.py
@@ -1,11 +1,16 @@
-import logging 
+import platform
+import logging
 
 logger = logging.getLogger('discord-bot')
 logger.setLevel(logging.DEBUG)
 
-file_handler = logging.FileHandler('./unseen-servant.log')
+if platform.system() == 'Linux':
+    handler = logging.handlers.SysLogHandler(address = '/dev/log')
+else:
+    handler = logging.FileHandler('./invisible-servant.log')
+
 formatter = logging.Formatter("%(asctime)s;%(levelname)s;%(message)s", "%Y-%m-%d %H:%M:%S")
 
-file_handler.setLevel(logging.DEBUG)
-file_handler.setFormatter(formatter)
-logger.addHandler(file_handler)
+handler.setLevel(logging.DEBUG)
+handler.setFormatter(formatter)
+logger.addHandler(handler)
