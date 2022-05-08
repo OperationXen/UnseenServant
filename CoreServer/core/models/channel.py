ec65b0bd82175509b931a9a65a6ebfc6e498b070
OperationXen <OperationXen@apocryphasecurity.com>
2022-05-08 12:24:59 +0100

    Started setting out channel model

---
+++ CoreServer/core/models/channel.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.utils import timezone
+
+from game import Game
+
+
+class GameChannel(models.Model):
+    """Object that defines a game channel"""
+
+    game = models.ForeignKey(Game, on_delete=models.CASCADE, related_name="channel")
+    discord_id = models.CharField(null=True, blank=True, help_text="Discord channel ID")
+    link = models.URLField(null=True, blank=True, help_text="Link to the channel on discord")
