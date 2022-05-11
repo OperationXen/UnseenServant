30f7e69f0e1f28607bfe799f1f1cdcb7ad484d33
OperationXen <OperationXen@apocryphasecurity.com>
2022-05-11 18:09:29 +0100

    Added channel models

--- core/models/channel.py
+++ core/models/channel.py
@@ -1,13 +1,20 @@
 from django.db import models
 from django.utils import timezone
 
-from game import Game
+from .game import Game
 
 
 class GameChannel(models.Model):
     """Object that defines a game channel"""
 
-    game = models.ForeignKey(Game, on_delete=models.CASCADE, related_name="channel")
-    discord_id = models.CharField(null=True, blank=True, help_text="Discord channel ID")
+    class ChannelStatuses(models.TextChoices):
+        """ Internal class to define possible game types """
+        READY = 'Ready', ('Channel created')
+        REMINDED = 'Reminder sent', ('Members reminded of game')
+        WARNED = 'Warning sent', ('1 Hour ping sent')
+
+    game = models.ForeignKey(Game, on_delete=models.CASCADE, related_name="text_channel")
+    discord_id = models.CharField(null=True, blank=True, max_length=32, help_text="Discord channel ID")
     link = models.URLField(null=True, blank=True, help_text="Link to the channel on discord")
-    name = models.CharField(blank=False, default="Unnamed game")
+    name = models.CharField(blank=False, max_length=64, default="Unnamed game")
+    status = models.TextField(choices=ChannelStatuses.choices, max_length=32, default=ChannelStatuses.READY, help_text='Status of the channel') 
