a027f07a5bba99236bf2240cb9791ed3b7c6b274
OperationXen <operationxen@apocryphasecurity.com>
2022-06-09 20:17:01 +0100

    Changed path (#115)

--- CoreServer/config/wsgi.py
+++ CoreServer/config/wsgi.py
@@ -11,8 +11,8 @@ import os, sys
 
 from django.core.wsgi import get_wsgi_application
 
-sys.path.append('/opt/InvisibleServant/CoreServer/')
-sys.path.append('/opt/InvisibleServant/venv/lib/site-packages')
+sys.path.append('/opt/UnseenServant/CoreServer/')
+sys.path.append('/opt/UnseenServant/venv/lib/site-packages')
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = get_wsgi_application()
