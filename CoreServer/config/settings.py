2c962ed92a1a2dc22afe44ac76b4a53b3f5bebce
OperationXen <operationxen@apocryphasecurity.com>
2022-07-02 14:28:49 +0100

    Updated for unit test support (#123)

--- CoreServer/config/settings.py
+++ CoreServer/config/settings.py
@@ -6,10 +6,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
-
 RANDOM_KEY = "".join(choices(ascii_letters + digits, k=128))
 DJANGO_SECRET = getenv("DJANGO_SECRET")
 SECRET_KEY = DJANGO_SECRET or RANDOM_KEY
@@ -90,7 +86,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'database/db.sqlite3',
+        'NAME': BASE_DIR / 'database' / 'db.sqlite3',
     }
 }
 
