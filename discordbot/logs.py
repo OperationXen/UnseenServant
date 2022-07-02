445fec263f20c7b3f05a1357a95f2989c6d9e69e
OperationXen <OperationXen@apocryphasecurity.com>
2022-07-02 15:51:38 +0100

    Fixed logging

--- discordbot/logs.py
+++ discordbot/logs.py
@@ -4,10 +4,7 @@ import logging
 logger = logging.getLogger('discord-bot')
 logger.setLevel(logging.DEBUG)
 
-if platform.system() == 'Linux':
-    handler = logging.handlers.SysLogHandler(address = '/dev/log')
-else:
-    handler = logging.FileHandler('./invisible-servant.log')
+handler = logging.FileHandler('./database/discordbot.log')
 
 formatter = logging.Formatter("%(asctime)s;%(levelname)s;%(message)s", "%Y-%m-%d %H:%M:%S")
 
