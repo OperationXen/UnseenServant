26e525c8b572f8ddd0a837a215fa9325c0f68a04
OperationXen <operationxen@apocryphasecurity.com>
2022-07-02 14:39:54 +0100

    Moved code out of subfolder (#124)

---
+++ api/tests/test_basic.py
@@ -0,0 +1,8 @@
+from django.test import TestCase
+
+
+class TestStub(TestCase):
+    """ Basic test stub to house unit tests """
+    def test_unit_tests_execute_OK(self) -> None:
+        """ Just check that the tests are actually execute """
+        self.assertTrue(True)
