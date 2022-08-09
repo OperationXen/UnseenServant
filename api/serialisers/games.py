e0edbea4277f43beafd0e3bfe497c5812b5d9175
OperationXen <operationxen@apocryphasecurity.com>
2022-08-09 21:04:00 +0100

    Removed number waitlisted and number players (#149)
    Added discord_id to player field

--- api/serialisers/games.py
+++ api/serialisers/games.py
@@ -7,7 +7,7 @@ class PlayerSummarySerialiser(ModelSerializer):
     """ Serialise a player (basic, no personal data) """
     class Meta:
         model = Player
-        fields = ['discord_name', 'standby']
+        fields = ['discord_id', 'discord_name', 'standby']
 
 
 class GameSerialiser(ModelSerializer):
@@ -16,22 +16,9 @@ class GameSerialiser(ModelSerializer):
     dm_name = ReadOnlyField(source='dm.name')
     players = PlayerSummarySerialiser(many=True)
 
-    number_of_players = SerializerMethodField()
-    number_of_waitlisted = SerializerMethodField()
-
-    def get_number_of_players(self, game):
-        """ Retrieve the current number of players for a given game """
-        players = game.players.filter(standby=False)
-        return players.count()
-
-    def get_number_of_waitlisted(self, game):
-        """ Retrieve the current number of players waitlisted for the specified game """
-        players = game.players.filter(standby=True)
-        return players.count()
-
     class Meta:
         model = Game
-        fields = ['id', 'dm_name', 'name', 'module', 'realm', 'variant', 'description', 'players', 'number_of_players', 'number_of_waitlisted', 'max_players', 'level_min', 'level_max', 'warnings', 'channel', 'streaming', 'datetime_release', 'datetime_open_release', 'datetime', 'length']
+        fields = ['id', 'dm_name', 'name', 'module', 'realm', 'variant', 'description', 'players', 'max_players', 'level_min', 'level_max', 'warnings', 'channel', 'streaming', 'datetime_release', 'datetime_open_release', 'datetime', 'length']
 
 
 class GameCreationSerialiser(ModelSerializer):
