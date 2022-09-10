156c63236eca6643aa0b306e0c63104f27ced69f
OperationXen <operationxen@apocryphasecurity.com>
2022-09-10 14:11:25 +0100

    Added trade search (#157)

--- discordbot/commands/moonseacodex.py
+++ discordbot/commands/moonseacodex.py
@@ -1,14 +1,10 @@
-from discord import SelectOption
 from discord.commands import Option
 
-from discordbot.components.moonseacodex import MSCCharacterList
-from discordbot.utils.moonseacodex import get_character_string
+from discordbot.components.moonseacodex import MSCCharacterList, MSCTradeSearchResultsEmbed
 
-from config.settings import DISCORD_GUILDS, DISCORD_ADMIN_ROLES, DISCORD_SIGNUP_ROLES, DISCORD_DM_ROLES
+from config.settings import DISCORD_GUILDS
 from discordbot.bot import bot
-from discordbot.utils.moonseacodex import get_msc_characters
-from core.utils.players import get_player_credit_text, issue_player_bonus_credit
-
+from discordbot.utils.moonseacodex import get_msc_characters, get_msc_trade_search
 
 
 @bot.slash_command(guild_ids=DISCORD_GUILDS, description="Display a character from the Moonsea Codex")
@@ -22,3 +18,20 @@ async def character(ctx):
         view.message = await ctx.followup.send(f"Characters for {ctx.author} from the Moonsea Codex:", view=view, ephemeral=True)
     else:
         message = await ctx.followup.send(f"Cannot find any characters for you on Moonsea Codex, have you set your discord profile ID?")
+
+@bot.slash_command(guild_ids=DISCORD_GUILDS, description="Search items for trade on Moonsea Codex")
+async def trade_search(ctx, search: Option(str, 'Search term to use', required=True)):
+    """ Query moonsea codex for items available for trading """
+    await ctx.defer(ephemeral=True)
+
+    items = get_msc_trade_search(search)
+
+    results_embeds = []
+    num_results = len(items)
+    if not num_results:
+        return await ctx.followup.send(f"No matches for '{search}'.", ephemeral=True)
+    else:
+        for item in items[:10]:
+            embed = MSCTradeSearchResultsEmbed(item)
+            results_embeds.append(embed)
+        return await ctx.followup.send(f"{num_results} Results for '{search}'{', showing first 10:' if num_results > 10 else ':'}", embeds=results_embeds, ephemeral=True)
