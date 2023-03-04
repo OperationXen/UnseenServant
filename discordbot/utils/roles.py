54f4a714f7216e8e86627fe9418de93e5120b600
OperationXen <OperationXen@apocryphasecurity.com>
2023-03-04 21:51:20 +0000

    Added admin detection to game add / remove

--- discordbot/utils/roles.py
+++ discordbot/utils/roles.py
@@ -1,5 +1,8 @@
+from discord import User
+
 from discord.errors import Forbidden
 from discordbot.logs import logger as log
+from config.settings import DISCORD_ADMIN_ROLES
 
 async def grant_role_to_user(role_name, user):
     """ Give an arbitrarily named permission to a user """
@@ -21,3 +24,16 @@ async def set_user_dm_registered(user):
     """ Given a discord user, grant them the 'bot-registered' permission """
     result = await grant_role_to_user('bot-registered', user)
     return result
+
+def get_user_role_names(discord_user: User) -> list[str]:
+    """ Fetch a list of the users role names (slight hack - could use actual roles) """
+    user_role_names = [role.name for role in discord_user.roles]
+    return user_role_names
+
+def discord_user_is_admin(discord_user: User) -> bool:
+    """ Check if user has any of the specified admin permissions """
+    user_role_names = get_user_role_names(discord_user)
+    matching_roles = list(set(user_role_names) & set(DISCORD_ADMIN_ROLES))
+    if matching_roles:
+        return True
+    return False
