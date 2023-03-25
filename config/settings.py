7280ef177d8b48b01f21434c569ac830a76a58ca
OperationXen <operationxen@apocryphasecurity.com>
2023-03-25 01:00:06 +0000

    Fixed URLs (#246)

--- config/settings.py
+++ config/settings.py
@@ -30,8 +30,8 @@ else:
     SERVER_URI = 'http://127.0.0.1:8000'
 
 WEBAPP_URL = getenv("WEBAPP_URL", "http://127.0.0.1:3000")
-AUTH_DONE_URL = WEBAPP_URL + "/members/discord_auth_done/"
-AUTH_FAIL_URL = WEBAPP_URL + "/members/discord_auth_failed/"
+AUTH_DONE_URL = WEBAPP_URL + "/discord_auth_done/"
+AUTH_FAIL_URL = WEBAPP_URL + "/discord_auth_failed/"
 
 ALLOWED_HOSTS = ["127.0.0.1"] if SERVER else []
 CSRF_TRUSTED_ORIGINS = [f"https://{SERVER}"] if SERVER else []
