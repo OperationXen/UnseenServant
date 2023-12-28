2f983dabe5d45192a9fa7914582d364ac0139cb4
OperationXen <OperationXen@apocryphasecurity.com>
2023-12-28 16:31:52 +0000

    added command stub

--- discord_bot/commands/dm.py
+++ discord_bot/commands/dm.py
@@ -68,7 +68,7 @@ async def remove_player(ctx, user: Option(Member, "Player to remove from the gam
 @bot.slash_command(guild_ids=DISCORD_GUILDS, description="Forcibly add a player to a game")
 @has_any_role(*DISCORD_DM_ROLES, *DISCORD_ADMIN_ROLES)
 async def add_player(ctx, user: Option(Member, "Player to add to the game", required=True)):
-    """add a player from a game - should only be run in a game channel"""
+    """add a player to a game - should only be run in a game channel"""
     await ctx.response.defer(ephemeral=True)
     log.info(f"{ctx.author.name} used command /add_player {user.name} in channel {ctx.channel.name}")
     game = await async_get_game_for_channel(ctx.channel)
@@ -90,6 +90,30 @@ async def add_player(ctx, user: Option(Member, "Player to add to the game", requ
     log.info(f"Unable to add player {user.name} to game {game.name}")
     return await ctx.followup.send(f"Unable to add {user.name} to {game.name}")
 
+@bot.slash_command(guild_ids=DISCORD_GUILDS, description="Forcibly add a player to a game waitlist")
+@has_any_role(*DISCORD_DM_ROLES, *DISCORD_ADMIN_ROLES)
+async def add_waitlist(ctx, user: Option(Member, "Player to add to the waitlist", required=True)):
+    """add a player to a games waitlist - should only be run in a game channel"""
+    await ctx.response.defer(ephemeral=True)
+    log.info(f"{ctx.author.name} used command /add_waitlist {user.name} in channel {ctx.channel.name}")
+    game = await async_get_game_for_channel(ctx.channel)
+    if not game:
+        log.error(f"Channel {ctx.channel.name} has no associated game, command failed")
+        return await ctx.followup.send("This channel is not linked to a game", ephemeral=True)
+
+    if not do_dm_permissions_check(ctx.author, game):
+        return await ctx.followup.send("You are not the DM for this game", ephemeral=True)
+
+    added = await async_add_player_to_game(game, user, force=True)
+    if added:
+        await async_do_waitlist_updates(game)
+        await async_update_mustering_embed(game)
+        await async_update_game_listing_embed(game)
+        log.info(f"Added {user.name} to game {game.name} waitlist")
+
+        return await ctx.followup.send("Player added to waitlist", ephemeral=True)
+    log.info(f"Unable to add player {user.name} to waitlist {game.name}")
+    return await ctx.followup.send(f"Unable to add {user.name} to waitlist for {game.name}")
 
 @bot.slash_command(guild_ids=DISCORD_GUILDS, description="Tags all players in the party")
 @has_any_role(*DISCORD_DM_ROLES, *DISCORD_ADMIN_ROLES)
