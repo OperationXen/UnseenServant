57695ad02ce000b1e2d0880721907703aede3ada
OperationXen <operationxen@apocryphasecurity.com>
2024-01-28 17:47:37 +0000

    387 add me shortcut to patch (#389)
    * Added via TDD
    
    * Fixed broken test case
    
    * Switched to explicit routing to set pk regex

--- api/urls_api.py
+++ api/urls_api.py
@@ -7,15 +7,17 @@ from api.views.options import GameRealmOptionsViewset, GameVariantOptionsViewset
 from api.views.status import StatusViewSet
 from api.views.statistics import GameStatsViewSet, PlayerStatsViewSet, GeneralStatsViewSet, DetailedStatsViewSet
 
+view_actions = {"get": "details", "post": "create", "patch": "partial_update", "delete": "delete"}
+
 router = DefaultRouter()
 router.register(r"games", GamesViewSet, basename="games")
 router.register(r"game/variants", GameVariantOptionsViewset, basename="game-variants")
 router.register(r"game/realms", GameRealmOptionsViewset, basename="game-realms")
-router.register(r"dungeonmasters", DMProfileViewset, basename="dms")
-
 
 urlpatterns = [
     path(r"", include(router.urls)),
+    re_path("dungeonmasters/(?P<pk>\w+)/?", DMProfileViewset.as_view(view_actions), name="dms-detail"),
+    re_path("dungeonmasters/?$", DMProfileViewset.as_view({"get": "list", "post": "create"}), name="dms-list"),
     re_path("status/?", StatusViewSet.as_view(), name="status"),
     re_path("statistics/games/?", GameStatsViewSet.as_view(), name="stats-games"),
     re_path("statistics/players/?", PlayerStatsViewSet.as_view(), name="stats-players"),
