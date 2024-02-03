a5300977832abfbb768a2592dabd7a8d71c59e38
OperationXen <OperationXen@apocryphasecurity.com>
2024-02-03 20:23:45 +0000

    Removed import

--- api/serialisers/games.py
+++ api/serialisers/games.py
@@ -1,5 +1,4 @@
 from rest_framework.serializers import ModelSerializer, ReadOnlyField, SerializerMethodField
-from rest_framework.fields import CurrentUserDefault
 
 from core.models import Game, Player
 
