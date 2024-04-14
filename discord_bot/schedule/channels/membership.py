9dfb21022bdf427e074b1b8b89dd86824e4cdbc6
OperationXen <OperationXen@apocryphasecurity.com>
2024-04-14 19:43:51 +0100

    Added exception handling

--- discord_bot/schedule/channels/membership.py
+++ discord_bot/schedule/channels/membership.py
@@ -1,18 +1,17 @@
 from typing import List
 
-from django.contrib.auth import get_user_model
 from discord.ext import tasks
 from discord.member import Member
+from discord.errors import NotFound
 
 from discord_bot.logs import logger as log
 from core.models.channel import GameChannel
+from core.models.auth import CustomUser
 
 from core.utils.channels import async_set_default_channel_membership
 from core.utils.channels import async_get_all_current_game_channels, async_get_game_channel_members
 from discord_bot.utils.channel import async_get_channel_current_members, refresh_discord_channel
 from discord_bot.utils.channel import async_add_discord_ids_to_channel, async_remove_discord_ids_from_channel
-
-UserModel = get_user_model()
 
 
 class ChannelMembershipController:
@@ -25,20 +24,23 @@ class ChannelMembershipController:
         self.guild = guild
         self.channel_event_loop.start()
 
-    def get_discord_ids(self, data: Member | UserModel) -> List[str]:
+    def get_discord_ids(self, data: Member | CustomUser) -> List[str]:
         """Return a list of discord IDs"""
         discord_ids = []
 
         for element in data:
             if type(element) == Member:
                 discord_ids.append(str(element.id))
-            if type(element) == UserModel and element.discord_id:
+            if type(element) == CustomUser and element.discord_id:
                 discord_ids.append(element.discord_id)
         return discord_ids
 
     async def sync_channel_membership(self, game_channel: GameChannel):
         """Update the channel membership to match that expected in the database state"""
-        discord_channel = await refresh_discord_channel(game_channel)
+        try:
+            discord_channel = await refresh_discord_channel(game_channel)
+        except NotFound:
+            return
 
         await async_set_default_channel_membership(game_channel)
         expected_members = await async_get_game_channel_members(game_channel)
@@ -60,10 +62,14 @@ class ChannelMembershipController:
 
     @tasks.loop(seconds=60)
     async def channel_event_loop(self):
-        if not self.initialised:
-            log.debug("[++] Starting up the Channel Membership Controller loop")
-            self.initialised = True
+        try:
+            if not self.initialised:
+                log.debug("[++] Starting up the Channel Membership Controller loop")
+                self.initialised = True
 
-        channels = await async_get_all_current_game_channels()
-        for channel in channels:
-            await self.sync_channel_membership(channel)
+            channels = await async_get_all_current_game_channels()
+            for channel in channels:
+                await self.sync_channel_membership(channel)
+        except Exception as e:
+            log.error(f"[!] An unhandled exception has occured in the Channel Membership Controller Loop")
+            self.initialised = False
