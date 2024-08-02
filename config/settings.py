28e4890bc5d6254bebb401ce087396575991e3b1
OperationXen <OperationXen@apocryphasecurity.com>
2024-08-02 20:44:37 +0100

    Configuration for database

--- config/settings.py
+++ config/settings.py
@@ -36,6 +36,11 @@ WEBAPP_URL = getenv("WEBAPP_URL", "http://127.0.0.1:3000")
 # Security Controls
 ALLOWED_HOSTS = ["127.0.0.1"]
 
+# Database config
+DB_NAME = getenv("DB_NAME", "database")
+DB_USER = getenv("DB_USER", "user")
+DB_PASS = getenv("DB_PASS", "password")
+
 # CORS_ALLOWED_ORIGINS = [WEBAPP_URL]
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_ALL_ORIGINS = True
@@ -128,9 +133,16 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": {
+        "ENGINE": "django.db.backends.postgres",
+        "HOST": "localhost",
+        "NAME": DB_NAME,
+        "USERNAME": DB_USER,
+        "PASSWORD": DB_PASS,
+    },
+    "local": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "database" / "db.sqlite3",
-    }
+    },
 }
 
 
