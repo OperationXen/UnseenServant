448b987a722a293fb0b5c9138d797f67a2136173
OperationXen <OperationXen@apocryphasecurity.com>
2024-08-03 18:12:22 +0100

    Improve logging

--- discord_bot/logs.py
+++ discord_bot/logs.py
@@ -4,9 +4,9 @@ logger = logging.getLogger("discord-bot")
 logger.setLevel(logging.DEBUG)
 
 try:
-    handler = logging.FileHandler("./database/discordbot.log")
+    handler = logging.FileHandler("./database/discordbot.log", encoding="utf8")
 except Exception as e:
-    handler = logging.FileHandler("./debugging.log")
+    handler = logging.FileHandler("./debugging.log", encoding="utf8")
 
 formatter = logging.Formatter("%(asctime)s;%(levelname)s;%(message)s", "%Y-%m-%d %H:%M:%S")
 
