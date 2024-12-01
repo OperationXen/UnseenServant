28bce0c871ae1e047cefd19a736d80ff23ed69d0
OperationXen <OperationXen@apocryphasecurity.com>
2024-12-01 13:58:28 +0000

    Added banlist to admin site

--- core/admin.py
+++ core/admin.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 
 from core.models import DM, CustomUser, Game, GameChannel, GameChannelMember
 from core.models import BonusCredit, Rank, Player, Strike, Ban, Announcement
+from core.models import DMBanList
 
 admin.site.register(CustomUser)
 admin.site.register(DM)
+admin.site.register(DMBanList)
 admin.site.register(Game)
 admin.site.register(GameChannel)
 admin.site.register(GameChannelMember)
