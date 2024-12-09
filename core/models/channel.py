1c2eeac13124442c01de83826d787f648f6e2ed8
OperationXen <OperationXen@apocryphasecurity.com>
2024-12-09 20:59:48 +0000

    Bot summary added

--- core/models/channel.py
+++ core/models/channel.py
@@ -4,6 +4,7 @@ from django.utils import timezone
 from .game import Game
 from .auth import CustomUser
 
+
 class GameChannel(models.Model):
     """Object that defines a game channel"""
 
@@ -13,16 +14,20 @@ class GameChannel(models.Model):
         READY = "Ready", ("Channel created")
         REMINDED = "Reminder sent", ("Members reminded of game")
         WARNED = "Warning sent", ("1 Hour ping sent")
+        SUMMARISED = "Game started", ("Session summary posted")
 
     game = models.ForeignKey(Game, on_delete=models.CASCADE, related_name="text_channel")
     discord_id = models.CharField(null=True, blank=True, max_length=32, help_text="Discord channel ID")
     link = models.URLField(null=True, blank=True, help_text="Link to the channel on discord")
     name = models.CharField(blank=False, max_length=64, default="Unnamed game")
     status = models.TextField(
-        choices=ChannelStatuses.choices, max_length=32, default=ChannelStatuses.READY, help_text="Status of the channel"
+        choices=ChannelStatuses.choices,
+        max_length=32,
+        default=ChannelStatuses.READY,
+        help_text="Status of the channel",
     )
-    
-    members = models.ManyToManyField(CustomUser, related_name="game_channels", through='gamechannelmember')
+
+    members = models.ManyToManyField(CustomUser, related_name="game_channels", through="gamechannelmember")
 
     class Meta:
         indexes = [
@@ -32,7 +37,8 @@ class GameChannel(models.Model):
 
     def __str__(self):
         return f"{self.name} [{self.status}]"
-    
+
+
 class GameChannelMember(models.Model):
     """Object that defines membership in a channel"""
 
@@ -44,6 +50,6 @@ class GameChannelMember(models.Model):
 
     channel = models.ForeignKey(GameChannel, on_delete=models.CASCADE, related_name="text_channel")
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="text_channel")
-    
+
     def __str__(self):
         return f"{self.channel} - {self.user}"
