06021a03d519580dc51f59fc8e99b61c38c2a23b
OperationXen <OperationXen@apocryphasecurity.com>
2025-05-24 22:44:23 +0100

    Trimmed out unused code

--- discord_bot/commands/admin.py
+++ discord_bot/commands/admin.py
@@ -1,15 +1,9 @@
-import discord
-from discord.commands import Option
 from discord.ext.commands import has_any_role
-from django.db import IntegrityError
-from django.forms import ValidationError
+
 
 import discord_bot.core
 from discord_bot.bot import bot
-from discord_bot.components.admin import AdminUserCreatedEmbed
-from config.settings import DISCORD_GUILDS, DISCORD_ADMIN_ROLES, DISCORD_DM_ROLES
-from core.utils.admin import async_create_new_dm_from_discord_user, async_create_new_admin_user
-from discord_bot.utils.roles import async_set_user_dm_registered
+from config.settings import DISCORD_GUILDS, DISCORD_ADMIN_ROLES
 
 
 @bot.slash_command(
