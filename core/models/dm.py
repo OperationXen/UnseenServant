3dd2ed7d93536fd9be089fc51741d4390339d128
OperationXen <OperationXen@apocryphasecurity.com>
2025-05-24 22:29:46 +0100

    Removed discord identifiers from DM object

--- core/models/dm.py
+++ core/models/dm.py
@@ -7,8 +7,6 @@ class DM(models.Model):
     """Representation of a DM"""
 
     name = models.CharField(max_length=64, help_text="DM's chosen alias or handle")
-    discord_id = models.CharField(null=True, blank=True, max_length=32, help_text="Discord ID of DM")
-    discord_name = models.CharField(blank=True, max_length=32, help_text="Discord username")
     description = models.TextField(blank=True, null=True, help_text="Flavour text / details to show")
     rules_text = models.TextField(blank=True, null=True, max_length=1024, help_text="Any banned items / spells, etc")
     muster_text = models.TextField(
@@ -22,7 +20,6 @@ class DM(models.Model):
     class Meta:
         verbose_name = "DM"
         verbose_name_plural = "DMs"
-        indexes = [models.Index(fields=["name", "discord_id"])]
 
     def __str__(self):
         return f"{self.name}"
