eca2eae0b3946d2158d8372879dd95df38a347d6
Daemoxia <operationxen@apocryphasecurity.com>
2025-07-23 21:07:16 +0100

    Added a custom filter for games (#656)
    * Added a custom filter for games
    
    * Newline at end of file
    
    ---------
    
    Co-authored-by: Daemoxia <Daemoxia@NotInventedHere>

--- core/admin/__init__.py
+++ core/admin/__init__.py
@@ -4,6 +4,8 @@ from core.models import DM, CustomUser, Game, GameChannel, GameChannelMember
 from core.models import BonusCredit, Rank, Player, Strike, Ban, Announcement
 from core.models import DMBanList
 
+from core.admin.admin_players import PlayerAdmin
+
 admin.site.register(CustomUser)
 admin.site.register(DM)
 admin.site.register(DMBanList)
@@ -11,7 +13,7 @@ admin.site.register(Game)
 admin.site.register(GameChannel)
 admin.site.register(GameChannelMember)
 
-admin.site.register(Player)
+admin.site.register(Player, PlayerAdmin)
 admin.site.register(BonusCredit)
 admin.site.register(Rank)
 admin.site.register(Strike)
