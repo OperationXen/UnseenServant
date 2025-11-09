8597586db40aa01d32bac11a1f38313f03257606
Daemoxia <operationxen@apocryphasecurity.com>
2025-11-09 12:19:17 +0000

    Added error boundary and truncation (#680)
    Co-authored-by: Daemoxia <Daemoxia@NotInventedHere>

--- discord_bot/commands/moonseacodex.py
+++ discord_bot/commands/moonseacodex.py
@@ -19,12 +19,16 @@ async def character(ctx):
     characters = get_msc_characters(discord_id=discord_name)
     log.info(f"[-] {len(characters or [])} characters found for {ctx.author.name}")
     if characters:
-        view = MSCCharacterList(ctx.author, characters)
+        try:
+            view = MSCCharacterList(ctx.author, characters)
+        except Exception as e:
+            log.error(f"[!] Exception {str(e)} occured creating MSC character list view for {discord_name}")
+            await ctx.followup.send("Error occured building list of your characters - you may have too many!", ephemeral=True)
         view.message = await ctx.followup.send(
             f"Characters for discord user {discord_name} from the Moonsea Codex:", view=view, ephemeral=True
         )
     else:
-        message = await ctx.followup.send(
+            await ctx.followup.send(
             f"Cannot find any characters for you on Moonsea Codex, have you set your discord profile ID?"
         )
 
