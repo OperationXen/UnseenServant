a61175f6c1b2165a3d37db980d78723d57320dc4
Daemoxia <OperationXen@NotInventedHere>
2026-03-15 22:14:50 +0000

    Moved embed controller to a cog

--- discord_bot/bot.py
+++ discord_bot/bot.py
@@ -1,9 +1,14 @@
 from discord import Intents
 from discord.ext import commands
 
+from discord_bot.schedule.embeds import EmbedController
+
 # Required to get permissions to query channel memberships
 intents = Intents.default()
 intents.message_content = True
 intents.members = True
 
 bot = commands.Bot(intents=intents)
+
+# Register cogs
+bot.add_cog(EmbedController)
