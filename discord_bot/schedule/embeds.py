702d4ce47d098c18676b862102aa0282a0850110
Daemoxia <OperationXen@NotInventedHere>
2026-03-15 22:14:50 +0000

    Moved embed controller to a cog

--- discord_bot/schedule/embeds.py
+++ discord_bot/schedule/embeds.py
@@ -1,18 +1,27 @@
-from discord.ext import tasks
+from discord.ext import tasks, commands
 
 from discord_bot.logs import logger as log
 from discord_bot.bot import bot
 
 
-class EmbedController:
+class EmbedController(commands.Cog):
     def __init__(self):
         """initialisation function"""
-        self.embed_refresh_loop.start()
+        self.worker.start()
+        log.info("[+] Started service: Embed auto update worker")
+
+    def cog_unload(self):
+        """cleanup function"""
+        self.worker.cancel()
 
     @tasks.loop(seconds=60)
-    async def embed_refresh_loop(self):
+    async def worker(self):
         try:
             for view in bot.persistent_views:
                 await view.update_message()
         except Exception as e:
             log.error(f"[!] An unhandled exception has occured in the EmbedManager Loop: " + str(e))
+
+    @worker.before_loop
+    async def before_loop_start(self):
+        await self.bot.wait_until_ready()
