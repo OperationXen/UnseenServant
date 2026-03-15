99e7327ba28266f95eb186225acb167ed435cc1b
Daemoxia <OperationXen@NotInventedHere>
2026-03-15 22:45:42 +0000

    Passed in important config

---
+++ discord_bot/schedule/channels/__init__.py
@@ -0,0 +1,2 @@
+from .manager import ChannelController
+from .membership import ChannelMembershipController
--- discord_bot/schedule/embeds.py
+++ discord_bot/schedule/embeds.py
@@ -1,14 +1,15 @@
 from discord.ext import tasks, commands
 
 from discord_bot.logs import logger as log
-from discord_bot.bot import bot
 
 
 class EmbedController(commands.Cog):
-    def __init__(self):
+    bot = None
+
+    def __init__(self, bot):
         """initialisation function"""
+        self.bot = bot
         self.worker.start()
-        log.info("[+] Started service: Embed auto update worker")
 
     def cog_unload(self):
         """cleanup function"""
@@ -17,7 +18,7 @@ class EmbedController(commands.Cog):
     @tasks.loop(seconds=60)
     async def worker(self):
         try:
-            for view in bot.persistent_views:
+            for view in self.bot.persistent_views:
                 await view.update_message()
         except Exception as e:
             log.error(f"[!] An unhandled exception has occured in the EmbedManager Loop: " + str(e))
@@ -25,3 +26,4 @@ class EmbedController(commands.Cog):
     @worker.before_loop
     async def before_loop_start(self):
         await self.bot.wait_until_ready()
+        log.info("[+] Starting service: Embed auto update worker")
